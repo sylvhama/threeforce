d1f7a429daf04e03122fa769b422afe7b3537e2e
Sylvain Hamann <sylvain.hamann@shopify.com>
2021-12-01 23:32:33 -0500

    initial commit

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
+import useSound from "use-sound";
 
 import { Triforce } from "./components";
 
+// @ts-ignore
+import titleSound from "./components/Triforce/title.mp3";
+
 export function Scene() {
-  return (
+  const [play] = useSound(titleSound);
+
+  const [start, setStart] = useState(false);
+
+  return start ? (
     <Canvas camera={{ position: [0, 0, -100] }}>
       <pointLight position={[0, 0, -200]} />
-      <Triforce />
+      <Triforce play={play} />
     </Canvas>
+  ) : (
+    <button type="button" onClick={() => setStart(true)}>
+      Start
+    </button>
   );
 }
