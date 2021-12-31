bbbca41f0bdebb55d73a3d61458fc430c65b24c1
Sylvain Hamann <sylvain.hamann@shopify.com>
2021-12-30 19:38:25 -0500

    move to react-spring

--- src/components/Triforce/Triforce.tsx
+++ src/components/Triforce/Triforce.tsx
@@ -1,104 +1,31 @@
-import { useState, useRef, useEffect } from "react";
-import { useFrame } from "@react-three/fiber";
-
 import { Triangle } from "./components";
-
-interface Position {
-  originX: number;
-  originY: number;
-  destinationX: number;
-  destinationY: number;
-}
-
-function createPosition(
-  originX: number,
-  originY: number,
-  destinationX: number,
-  destinationY: number
-): Position {
-  return {
-    originX,
-    originY,
-    destinationX,
-    destinationY,
-  };
-}
-
-const topPosition = createPosition(0, 104, 0, 24);
-const leftPosition = createPosition(89, -72, 9, 6);
-const rightPosition = createPosition(-89, -72, -9, 6);
-
-function useTrianglePosition(
-  position: Position,
-  down = true,
-  left = true
-): [React.RefObject<THREE.Mesh>, boolean] {
-  const [hasFinished, setHasFinished] = useState(false);
-  const ref = useRef<THREE.Mesh>(null);
-
-  useFrame(() => {
-    if (ref.current) {
-      const step = 0.25;
-      const stepY = down ? -step : step;
-      const stepX = left ? -step : step;
-
-      if (ref.current.position.y !== position.destinationY) {
-        ref.current.position.y = ref.current.position.y + stepY;
-        ref.current.rotateY(0.08);
-      }
-
-      if (ref.current.position.x !== position.destinationX) {
-        ref.current.position.x = ref.current.position.x + stepX;
-      }
-
-      if (
-        ref.current.position.y === position.destinationY &&
-        ref.current.position.x === position.destinationX
-      ) {
-        ref.current.rotation.y = 0;
-        setHasFinished(true);
-      }
-    }
-  });
-
-  return [ref, hasFinished];
-}
+import { useSpring } from "@react-spring/three";
 
 interface Props {
   onFinish(): void;
 }
 
 export function Triforce({ onFinish }: Props) {
-  const [topRef, topHasFinished] = useTrianglePosition(topPosition);
-  const [leftRef, leftHasFinished] = useTrianglePosition(leftPosition, false);
-  const [rightRef, rightHasFinished] = useTrianglePosition(
-    rightPosition,
-    false,
-    false
-  );
-
-  useEffect(() => {
-    if (topHasFinished && leftHasFinished && rightHasFinished) {
-      onFinish();
-    }
-  }, [topHasFinished, leftHasFinished, rightHasFinished]);
+  useSpring<Animation>({
+    to: {
+      value: 1,
+    },
+    from: { value: 0 },
+    config: {
+      duration: 5200,
+    },
+    onRest: onFinish,
+  });
 
   return (
     <group position={[0, -5, 0]}>
+      <Triangle originX={0} originY={104} destinationX={0} destinationY={24} />
+      <Triangle originX={89} originY={-72} destinationX={9} destinationY={6} />
       <Triangle
-        originX={topPosition.originX}
-        originY={topPosition.originY}
-        ref={topRef}
-      />
-      <Triangle
-        originX={leftPosition.originX}
-        originY={leftPosition.originY}
-        ref={leftRef}
-      />
-      <Triangle
-        originX={rightPosition.originX}
-        originY={rightPosition.originY}
-        ref={rightRef}
+        originX={-89}
+        originY={-72}
+        destinationX={-9}
+        destinationY={6}
       />
     </group>
   );
--- src/components/Triforce/components/Triangle/Triangle.tsx
+++ src/components/Triforce/components/Triangle/Triangle.tsx
@@ -1,34 +1,58 @@
 import { Shape } from "three";
-import { forwardRef } from "react";
+import { useSpring, animated } from "@react-spring/three";
 
 interface Props {
   originX: number;
   originY: number;
+  destinationX: number;
+  destinationY: number;
   length?: number;
 }
 
-export const Triangle = forwardRef<THREE.Mesh, Props>(
-  ({ originX, originY, length = 8 }, ref) => {
-    const shape = new Shape()
-      .moveTo(0, 0)
-      .lineTo(-length, length * -2)
-      .lineTo(length, length * -2)
-      .lineTo(0, 0);
+interface Animation {
+  position: [x: number, y: number, z: number];
+  rotationY: number;
+  scale: number;
+}
 
-    const extrudeSettings = {
-      depth: 4,
-      bevelEnabled: true,
-      bevelSegments: 2,
-      steps: 2,
-      bevelSize: 1,
-      bevelThicksness: 1,
-    };
+export const Triangle = ({
+  originX,
+  originY,
+  destinationX,
+  destinationY,
+  length = 8,
+}: Props) => {
+  const shape = new Shape()
+    .moveTo(0, 0)
+    .lineTo(-length, length * -2)
+    .lineTo(length, length * -2)
+    .lineTo(0, 0);
 
-    return (
-      <mesh ref={ref} position={[originX, originY, 0]}>
-        <extrudeGeometry args={[shape, extrudeSettings]} />
-        <meshStandardMaterial color="#eec036" />
-      </mesh>
-    );
-  }
-);
+  const extrudeSettings = {
+    depth: 4,
+    bevelEnabled: true,
+    bevelSegments: 2,
+    steps: 2,
+    bevelSize: 1,
+    bevelThicksness: 1,
+  };
+
+  const { position, rotationY, scale } = useSpring<Animation>({
+    to: {
+      position: [destinationX, destinationY, 0],
+      rotationY: Math.PI * 10,
+      scale: 1,
+    },
+    from: { position: [originX, originY, 0], rotationY: 0, scale: 0.25 },
+    config: {
+      duration: 5300,
+    },
+  });
+
+  return (
+    <animated.mesh position={position} rotation-y={rotationY} scale={scale}>
+      <extrudeGeometry args={[shape, extrudeSettings]} />
+      <meshStandardMaterial color="#eec036" />
+    </animated.mesh>
+  );
+};
